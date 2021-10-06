6ea371b5f53a1c37929f984d4d51124799b8001f
Efren Gaxiola <efren.gaxiola123@gmail.com>
2021-10-05 20:57:34 -0700

    Actualizacion de 2 actividad

--- main.py
+++ main.py
@@ -13,4 +13,26 @@ def Humanguess():
         print("El numero es mayor")
 
 Humanguess()
+
+def computerGuess():
+    number = random.randrange(0,10)
+    bandera =False
+
+    while (bandera == False):
+        print ("Este es tu numero? "+number)
+        opc = input("Si | No: ").lower()
+        if(opc == "no"):
+            print("Es mayor o menor?")
+            opc2 = input("Mayor | Menor: ").lower()
+            if (opc2 == "menor"):
+                newNumber =random.randrange(0,number)
+                number = newNumber
+            else:
+                newNumber = random.randrange(number,10)
+                number = newNumber
+        else:
+            bandera = True
+
+computerGuess()            
+
        
