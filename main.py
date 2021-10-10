821b32a8259aea04f7eee0a313b044228c5d3ed6
Efren Gaxiola <efren.gaxiola123@gmail.com>
2021-10-10 12:09:20 -0700

    Juego terminado

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
 import random
-def Humanguess(): 
-    number =random.randrange(0,10)
-    print(number)
-    numero=-1
-    while numero!=number:
-     numero=int(input("Adivina el numero que estoy pensando: "))
-     if number==numero:
-        print(f"Felicidades, el numero era: {number}")
-     elif numero > number:
-        print("Te pasaste el numero es menor")
-     else:
-        print("El numero es mayor")
+#def Humanguess(): 
+    #number =random.randrange(0,10)
+    #print(number)
+    #numero=-1
+    #while numero!=number:
+     #numero=int(input("Adivina el numero que estoy pensando: "))
+     #if number==numero:
+     #   print(f"Felicidades, el numero era: {number}")
+    # elif numero > number:
+   #     print("Te pasaste el numero es menor")
+  #   else:
+ #       print("El numero es mayor")
 
-Humanguess()
+#Humanguess()
 
 def computerGuess():
-    number = random.randrange(0,10)
+    #number = random.randrange(0,10)
     bandera =False
+    NumMax=100
+    NumMin=0
+    number=random.randrange(NumMin,NumMax)
 
     while (bandera == False):
-        print ("Este es tu numero? "+number)
+        print(number)
+        print ("Este es tu numero? ")
         opc = input("Si | No: ").lower()
         if(opc == "no"):
             print("Es mayor o menor?")
             opc2 = input("Mayor | Menor: ").lower()
             if (opc2 == "menor"):
-                newNumber =random.randrange(0,number)
+                NumMax=number
+                newNumber =random.randrange(NumMin,NumMax)
                 number = newNumber
             else:
-                newNumber = random.randrange(number,10)
+                NumMin=number
+                newNumber = random.randrange(NumMin,NumMax)
                 number = newNumber
         else:
             bandera = True
+            print("¡Eres muy bueno, has ganado!")
 
 computerGuess()            
 
